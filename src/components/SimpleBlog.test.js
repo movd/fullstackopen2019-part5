94990583414e4ddc6b565d77ff21ad77aa1750ee
Moritz <22394314+movd@users.noreply.github.com>
2019-12-12 13:09:46 +0100

    Finish Ex. 5.14

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import SimpleBlog from "./SimpleBlog";
 // import { exportAllDeclaration } from "@babel/types";
 
-test("render blog (title, author and likes)", () => {
-  const blog = {
-    title: "A 10-Step Guide To Making Your Best Technical Talk Yet",
-    author: "Matthew Jones",
-    likes: 25
-  };
+const blog = {
+  title: "A 10-Step Guide To Making Your Best Technical Talk Yet",
+  author: "Matthew Jones",
+  likes: 25
+};
 
+test("render blog (title, author and likes)", () => {
   const component = render(<SimpleBlog blog={blog} />);
 
   const title = component.container.querySelector(".title");
@@ -22,4 +22,18 @@ test("render blog (title, author and likes)", () => {
   expect(likes).toHaveTextContent(blog.likes);
 
   // component.debug();
+});
+
+test("clicking the like button two times calls the event handler two times", () => {
+  const mockHandler = jest.fn();
+
+  const component = render(<SimpleBlog blog={blog} onClick={mockHandler} />);
+
+  const likeButton = component.container.querySelector("button");
+
+  for (let i = 0; i < 2; i++) {
+    fireEvent.click(likeButton);
+  }
+
+  expect(mockHandler.mock.calls.length).toBe(2);
 });
